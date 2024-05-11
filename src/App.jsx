51525bb8b0cdde57feb89a77e0ab109c72782e54
kauã gomes <kgs26121967@gmail.com>
2024-05-11 17:32:32 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <form>
+        <input type="number" placeholder='Digite sua altura'/>
+        <input type="number" placeholder='Digite seu peso'/>
+      </form>
+    </>
+  )
+}
+
+export default App
